177a1e1e1c86c597f88384eee30f2f38938c5bce
alesahe <confuzzlementation@gmail.com>
2025-06-03 15:58:02 +1000

    added more

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.get("/contact", function (req, res) {
 app.get("/login", function (req, res) {
   res.sendFile(path.join(__dirname, "public/html/login.html"));
 });
+app.get("/signup", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/html/signup.html"));
+});
 
 //post requests
 
